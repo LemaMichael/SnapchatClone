4a215d7e2ae359fa1ed7f84a781e32540a4fb755
Michael Lema <Mel526@nyu.edu>
2017-07-30 03:28:56 -0400

    Initial Commit

---
+++ Snapchat Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat Clone
+//
+//  Created by Michael Lema on 7/30/17.
+//  Copyright © 2017 Michael Lema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
