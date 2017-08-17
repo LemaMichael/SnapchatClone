73654c7efc520e5e208f61cc9480b6d3f34e6195
Michael Lema <Mel526@nyu.edu>
2017-08-17 15:33:36 -0400

    Learned how to read a JSON file that contained country codes.

---
+++ Snapchat Clone/CountryCode.swift
@@ -0,0 +1,16 @@
+//
+//  CountryCode.swift
+//  Snapchat Clone
+//
+//  Created by Michael Lema on 8/17/17.
+//  Copyright © 2017 Michael Lema. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CountryCode: Decodable {
+    let name: String
+    let dial_code: String
+    let code: String
+}
--- Snapchat Clone/CountryController.swift
+++ Snapchat Clone/CountryController.swift
@@ -11,26 +11,45 @@ import UIKit
 
 class CountryController: UITableViewController {
     static let cellId = "cellId"
+    var countryCodes = [CountryCode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         adjustNavBar()
         tableView.register(countryCell.self, forCellReuseIdentifier: CountryController.cellId)
+        
+        if let path = Bundle.main.path(forResource: "CountryCodes", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe) as Data
+                do {
+                    let areaCode = try JSONDecoder().decode([CountryCode].self, from: jsonData)
+                    //print(areaCode)
+                    countryCodes = areaCode.sorted { $0.name < $1.name }
+                } catch let err {
+                    print(err.localizedDescription)
+                }
+            } catch let err {
+                print(err.localizedDescription)
+            }
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 25
+        return countryCodes.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CountryController.cellId, for: indexPath) as! countryCell
+        let areaCode = NSString(format:"(%@)", countryCodes[indexPath.row].dial_code) as String
+        cell.countryLabel.text = countryCodes[indexPath.row].name + " " + areaCode
+
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        return 55
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -62,23 +81,21 @@ class CountryController: UITableViewController {
     }
 }
 
+//: MARK: - Cell for table view
 class countryCell: UITableViewCell {
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setUpViews()
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    let countryLabel: UILabel = {
+    lazy var countryLabel: UILabel = {
         let label = UILabel()
         label.text = "Afghanistan (+93)"
-        label.font = UIFont(name: "Avenir-Book", size: 20)
+        label.font = UIFont(name: "Avenir Next-Regular", size: 14)
         return label
     }()
-    
     func setUpViews() {
         addSubview(countryLabel)
         addConstraintsWithFormat(format: "H:|-16-[v0]|", views: countryLabel)
