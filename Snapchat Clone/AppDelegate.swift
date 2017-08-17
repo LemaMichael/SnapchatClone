b5c8551b4f20c26fde6448ece186a5a1af2ed8e0
Michael Lema <Mel526@nyu.edu>
2017-08-17 03:17:57 -0400

    created Table view cell.

--- Snapchat Clone/AppDelegate.swift
+++ Snapchat Clone/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         //window?.rootViewController = UINavigationController(rootViewController: WelcomeController())
-        window?.rootViewController = UINavigationController(rootViewController: CountryController())
+        window?.rootViewController = UINavigationController(rootViewController: PhoneController())
         return true
     }
 
--- Snapchat Clone/CountryController.swift
+++ Snapchat Clone/CountryController.swift
@@ -10,36 +10,78 @@ import Foundation
 import UIKit
 
 class CountryController: UITableViewController {
-    static let celld = "cellId"
+    static let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //setUpNavigationBar(leftImage: "BackButton")
-
-        //: Change back the shadowImage to default
-        self.navigationController?.navigationBar.shadowImage = nil
-        navigationItem.title = "Select Country"
-        navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.black, NSFontAttributeName: UIFont(name: "Avenir-Medium", size: 20)!]
+        adjustNavBar()
+        tableView.register(countryCell.self, forCellReuseIdentifier: CountryController.cellId)
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CountryController.cellId, for: indexPath) as! countryCell
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        popCurrentView()
+    }
+    
+    //: TODO: Increase navBar height by 20 points
+    func adjustNavBar() {
+        setUpNavigationBar(leftImage: "BackButton")
+        guard let navController = self.navigationController else {
+            return
+        }
+        //: Change back the shadowImage to default
+        navController.navigationBar.shadowImage = nil
+        navController.navigationBar.isTranslucent = false
+        navigationItem.title = "Select Country"
+        navController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.black, NSFontAttributeName: UIFont(name: "Avenir-Medium", size: 20)!]
+    }
+    
+    //: FIXME: - Make the status bar be white
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = false
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.isNavigationBarHidden = true
     }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-    }
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
 
+class countryCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let countryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Afghanistan (+93)"
+        label.font = UIFont(name: "Avenir-Book", size: 20)
+        return label
+    }()
+    
+    func setUpViews() {
+        addSubview(countryLabel)
+        addConstraintsWithFormat(format: "H:|-16-[v0]|", views: countryLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: countryLabel)
+    }
+}
