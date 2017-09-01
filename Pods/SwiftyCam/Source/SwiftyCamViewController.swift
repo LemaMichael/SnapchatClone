d0d8135dd0098cc374ce6ffbe9e3b550f652b6ba
Michael Lema <Mel526@nyu.edu>
2017-09-01 14:12:23 -0400

    Handle Privacy Settings when app launches.

--- Pods/SwiftyCam/Source/SwiftyCamViewController.swift
+++ Pods/SwiftyCam/Source/SwiftyCamViewController.swift
@@ -378,9 +378,20 @@ open class SwiftyCamViewController: UIViewController {
 			case .configurationFailed:
 				// Unknown Error
 				DispatchQueue.main.async(execute: { [unowned self] in
-					let message = NSLocalizedString("Unable to capture media", comment: "Alert message when something goes wrong during capture session configuration")
-					let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
-					alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"), style: .cancel, handler: nil))
+					let message = "Snapchat Clone is a camera app! 😜 To continue, you'll need to allow Camera access in Settings"
+					let alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
+                    let openSettingsAction = UIAlertAction(title: "Open Settings", style: .default, handler: { (_) in
+                        //: Go to settings
+                        if let settingsURL = URL(string: UIApplicationOpenSettingsURLString) {
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(settingsURL)
+                            }
+                        }
+                    })
+					alertController.addAction(openSettingsAction)
+
 					self.present(alertController, animated: true, completion: nil)
 				})
 			}
@@ -861,8 +872,8 @@ open class SwiftyCamViewController: UIViewController {
 		// prompt User with UIAlertView
 
 		DispatchQueue.main.async(execute: { [unowned self] in
-			let message = NSLocalizedString("AVCam doesn't have permission to use the camera, please change privacy settings", comment: "Alert message when the user has denied access to the camera")
-			let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
+			let message = NSLocalizedString("Snapchat Clone doesn't have permission to use the camera, please change privacy settings", comment: "Alert message when the user has denied access to the camera")
+			let alertController = UIAlertController(title: "SnapChat Clone", message: message, preferredStyle: .alert)
 			alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"), style: .cancel, handler: nil))
 			alertController.addAction(UIAlertAction(title: NSLocalizedString("Settings", comment: "Alert button to open Settings"), style: .default, handler: { action in
 				if #available(iOS 10.0, *) {
